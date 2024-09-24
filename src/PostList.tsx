b04f7b023e9167773bcb3a6c1bebb82864a080cd
Wooyeol Lee <wooyeol0519@snu.ac.kr>
2024-09-25 02:27:22 +0900

    feat: use ScrollArea for Posts

--- src/PostList.tsx
+++ src/PostList.tsx
@@ -5,6 +5,7 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
 import { type Post } from './App';
 
@@ -15,23 +16,25 @@ type PostListProps = {
 
 export const PostList = ({ posts, onPostClickBuilder }: PostListProps) => {
   return (
-    <div>
-      <h1 className="text-5xl">Post List</h1>
-      {posts.map((post) => (
-        <Card
-          key={post.id}
-          className="m-1 hover:opacity-50"
-          onClick={onPostClickBuilder(post.id)}
-        >
-          <CardHeader>
-            <CardTitle>{post.title}</CardTitle>
-            <CardDescription>작성자: {post.userId}</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>{post.body}</p>
-          </CardContent>
-        </Card>
-      ))}
+    <div className="m-8">
+      <h1 className="text-4xl">포스트 목록</h1>
+      <ScrollArea className="h-svh overflow-y-auto">
+        {posts.map((post) => (
+          <Card
+            key={post.id}
+            className="m-1 hover:opacity-50"
+            onClick={onPostClickBuilder(post.id)}
+          >
+            <CardHeader>
+              <CardTitle>{post.title}</CardTitle>
+              <CardDescription>작성자: {post.userId}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>{post.body}</p>
+            </CardContent>
+          </Card>
+        ))}
+      </ScrollArea>
     </div>
   );
 };
